cec8f6d203a74536e22eb423f74f1a40cd43e9de
Pragadeesh Nehru <pragadeeshxenocrate@gmail.com>
2024-08-26 14:14:27 +0530

    modified:   src/components/About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div id="about" className="mb-32 mt-10">
       <div className="container mx-auto px-6  text-white comfortaa-light flex flex-row gap-5">
-        <p className="text-2xl text-justify flex flex-col justify-center ">
+        <p className="text-2xl ">
           A multifaceted individual, I am a computer science student and
           developer with a passion for coding, creativity in art, a love for
           reading, and a flair for writing. Always learning, always creating.
--- src/components/FlipWord.jsx
+++ src/components/FlipWord.jsx
@@ -4,16 +4,17 @@ export default function FlipWord() {
   const words = ["Web Developer", "UX Designer", "Artist", "Writer"];
 
   return (
-    <div className="sm:h-[6rem] md:h-[6rem] lg:h-[10rem] flex items-center justify-start container mx-auto px-6">
-      <div className="font-display flip-font font-normal text-neutral-600 dark:text-neutral-400 whitespace-nowrap">
-        An aspiring
-        <FlipWords
-          words={words}
-          duration={700}
-          className="comfortaa-light"
-        />
-        <br />
-      </div>
-    </div>
+    <div className="flex items-center justify-start container mx-auto px-6 sm:h-[4rem] md:h-[8rem] lg:h-[10rem]">
+  <div className="font-display flip-font font-normal text-neutral-600 dark:text-neutral-400 whitespace-nowrap text-[1.5rem] sm:text-[2rem] md:text-[2.5rem] lg:text-[3rem]">
+    An aspiring
+    <FlipWords
+      words={words}
+      duration={700}
+      className="comfortaa-light"
+    />
+    <br />
+  </div>
+</div>
+
   );
 }
